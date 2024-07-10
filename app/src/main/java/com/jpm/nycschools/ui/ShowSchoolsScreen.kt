a508d9ff1a2c698c13080d19afb5300e5075edba
Brandon Pulley <brandonpulley@gmail.com>
2024-07-09 18:09:18 -0700

    getting initial school list UI set up

--- app/src/main/java/com/jpm/nycschools/ui/ShowSchoolsScreen.kt
+++ app/src/main/java/com/jpm/nycschools/ui/ShowSchoolsScreen.kt
@@ -1,9 +1,51 @@
 package com.jpm.nycschools.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jpm.nycschools.models.School
 
 
 @Composable
-fun ShowSchoolsScreen() {
+fun ShowSchoolsScreen(schoolsList: List<School>, onClickSchool: (String) -> Unit) {
+    LazyColumn {
+        items(schoolsList.size) { index ->
+            SchoolView(school = schoolsList[index], onClickSchool)
+        }
+    }
+}
 
+@Composable
+fun SchoolView(school: School, onClickSchool: (String) -> Unit) {
+    Column(modifier = Modifier
+        .background(Color.DarkGray)
+        .padding(8.dp)
+        .clickable {
+            onClickSchool(school.dbn)
+        }) {
+        Text(
+            text = "school name: ${school.schoolName}",
+            color = Color.White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SchoolViewPreview() {
+    val school = School(schoolName = "Examplerary School")
+    MaterialTheme {
+        SchoolView(school = school) {
+            // no op
+        }
+    }
 }
