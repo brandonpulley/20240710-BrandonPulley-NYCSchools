f205f8a72b449b80d4b02d42ee61f4a43e2161f8
Brandon Pulley <brandonpulley@gmail.com>
2024-07-09 21:50:39 -0700

    got end to end happy path working

--- app/src/main/java/com/jpm/nycschools/MainActivity.kt
+++ app/src/main/java/com/jpm/nycschools/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import com.jpm.nycschools.ui.ShowSchoolsScreen
 import com.jpm.nycschools.viewmodels.NycSchoolsViewModel
@@ -13,14 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel: NycSchoolsViewModel by viewModels()
+        viewModel.retrieveAllData()
         setContent {
             MaterialTheme {
                 val uiState = viewModel.uiState.collectAsState()
                 if (uiState.value.chosenSchool != null) {
-
-                }
-                ShowSchoolsScreen(uiState.value.schoolList) { schoolId ->
-                    viewModel.updateChosenSchool(schoolId = schoolId)
+                    Column {
+                        Text(text = "Here's the chosen school name: ${uiState.value.chosenSchool?.schoolName}")
+                    }
+                } else {
+                    ShowSchoolsScreen(uiState.value.schoolList) { schoolId ->
+                        viewModel.updateChosenSchool(schoolId = schoolId)
+                    }
                 }
             }
         }
--- app/src/main/java/com/jpm/nycschools/viewmodels/NycSchoolsViewModel.kt
+++ app/src/main/java/com/jpm/nycschools/viewmodels/NycSchoolsViewModel.kt
@@ -1,11 +1,15 @@
 package com.jpm.nycschools.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.jpm.nycschools.datasources.NycSchoolsRemoteDataSource
 import com.jpm.nycschools.models.SatScore
 import com.jpm.nycschools.models.School
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class UiState(
     // the list of schools to be displayed
@@ -19,11 +23,52 @@ class NycSchoolsViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(UiState())
     val uiState = _uiState.asStateFlow()
 
-    private val schoolsSatScores: Map<String, SatScore> = mapOf()
-    private val schoolList: Map<String, School> = mapOf()
+    private val schoolsSatScores: HashMap<String, SatScore> = hashMapOf()
+    private val schoolList: HashMap<String, School> = hashMapOf()
+    private val dataSource: NycSchoolsRemoteDataSource = NycSchoolsRemoteDataSource()
 
-    fun retrieveSchoolList() {
+    private suspend fun retrieveSchoolList() {
+        val schoolsResponse = dataSource.retrieveNycSchoolsList()
+        if (schoolsResponse.isSuccessful) {
+            schoolsResponse.data.forEach { item ->
+                val school = item as School
+                schoolList[school.dbn] = school
+                refreshSchoolListUi()
+            }
+        } else {
+            // TODO: update UI for failure to get school response
+        }
+    }
 
+    private suspend fun retrieveSatScores() {
+        val satScoresResponse = dataSource.retrieveNycSatScoresList()
+        if (satScoresResponse.isSuccessful) {
+            satScoresResponse.data.forEach { item ->
+                val satSchool = item as SatScore
+                schoolsSatScores[satSchool.dbn] = satSchool
+            }
+        } else {
+            // TODO: update UI for failure to get sat response
+        }
+    }
+
+    fun retrieveAllData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            retrieveSchoolList()
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            retrieveSatScores()
+        }
+    }
+
+    fun refreshSchoolListUi() {
+        _uiState.update { currentState ->
+            UiState(
+                schoolList = schoolList.values.toList(),
+                chosenSchool = currentState.chosenSchool,
+                chosenSatSchoolInfo = currentState.chosenSatSchoolInfo
+            )
+        }
     }
 
     fun updateChosenSchool(schoolId: String?) {
